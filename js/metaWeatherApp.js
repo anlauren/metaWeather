141c70526c9f5def2f1b6532d59986e58c1bce7d
anlauren <antoine.lauren.f@gmail.com>
2016-01-27 17:25:43 +0100

    Angular data Binding set up.

--- js/metaWeatherApp.js
+++ js/metaWeatherApp.js
@@ -6,8 +6,18 @@
 var mtw = angular.module('mtw', []); // Creating the main ng-app metaweather (mtw) controller for rootscope
 
 /*************************************** The Modules *****************************************/
-mtw.controller('VarCtrl', ['$scope', function($scope){ // just a controller example for now
-	$scope.a = 0;
+mtw.controller('DataController', ['$scope', function($scope){ // Controlling the data we will send to the app and how
+	$scope.listLang = ['EN', 'FR', 'SP'];
+	$scope.listLocateType = ['State', 'Town', 'GPS'];
+	$scope.listMetric =['Imperial', 'Metric'];
+	$scope.listDays = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+	$scope.lang = 'EN';
+	$scope.locateType = 'Town';
+	$scope.metric ='Metric';
+	$scope.days = 4;
+	$scope.req='Austin';
+
+
 }]);
 
 /*************************************** The Servives **********************************/
